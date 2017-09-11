56f80818ea6991801be5d1f9d5bb7b1c6cd6f069
herringson <herringson@gmail.com>
2017-09-11 20:09:10 +0000

    Added rc.local and wpa_supplicant install to install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,39 @@
-echo -e [Changing deafult pi password-----------------------------------------------------]
+echo -e [Changing deafult pi password]
 echo ""
 echo ""
 	#passwd
 echo ""
 echo ""
-echo -e [Updating apt sources-------------------------------------------------------------]
+echo -e [Updating apt sources]
 echo ""
 echo ""
 	sudo apt-get update
 echo ""
 echo ""
-echo -e [installing pure data and git-----------------------------------------------------]
+echo -e [installing pure data and git]
 echo ""
 echo ""
 	sudo apt-get --assume-yes install puredata git
 echo ""
 echo ""
-echo -e [Updating Submodules--------------------------------------------------------------]
+echo -e [Updating Submodules]
 echo ""
 echo ""
 	git submodule update --init --recursive
 echo "" 
 echo ""
-#echo -e [Installing cmake----------------------------------------------------------------]
-#echo ""
-#echo ""
-#echo ""#sudo apt-get install --assume-yes cmake
-echo -e [building abl_link----------------------------------------------------------------]
+echo -e [building abl_link]
 echo ""
 echo ""
 	(cd abl_link/external; make)
 echo ""
 echo ""
+echo ""
+echo ""
+echo -e [Adding launchpd.sh to rc.local]
+	(sudo cd config_files; cp rc.local /etc/rc/local)
+echo ""
+echo ""
+echo -e [Installing wpa_supplicant.conf -> /etc/wpa_supplicant]
+	(sudo cd config_files; cp wpa_supplicant.conf /etc/wpa_supplicant)	
 
